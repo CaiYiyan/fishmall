f476848dc453cc134cc4c94244a348b7ac734219
Yan <cyiyangg@gmail.com>
2020-08-02 16:17:04 +0800

    ADD: init project

---
+++ fishmall-member/src/test/java/com/yan/fishmall/fishmallmember/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yan.fishmall.member;
+
+import org.junit.jupiter.api.Test66;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
