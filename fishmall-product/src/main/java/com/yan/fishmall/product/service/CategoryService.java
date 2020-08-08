e5154ba1b804a6b3a30bee39ca99fd1720a332d1
Yan <cyiyangg@gmail.com>
2020-08-08 17:37:50 +0800

    FIX: add logic delete for pms_category

--- fishmall-product/src/main/java/com/yan/fishmall/product/service/CategoryService.java
+++ fishmall-product/src/main/java/com/yan/fishmall/product/service/CategoryService.java
@@ -18,6 +18,10 @@ public interface CategoryService extends IService<CategoryEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
 
-	List<CategoryEntity> listWithTree();
+    List<CategoryEntity> listWithTree();
+
+	void removeMenuByIds(List<Long> asList);
+    
+
 }
 
--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,5 +1,24 @@
 <template>
-  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    :expand-on-click-node="false"
+    show-checkbox
+    node-key="catId"
+  >
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button v-if="node.level<=2" type="text" size="mini" @click="() => append(data)">Append</el-button>
+        <el-button
+          v-if="node.childNodes.length==0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+        >Delete</el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
@@ -39,18 +58,22 @@ export default {
   activated() {}, // 如果页面有keep-alive缓存功能，这个函数会触发
   // 方法集合
   methods: {
-    handleNodeClick(data) {
-      console.log(data);
-    },
     getMenus() {
-        this.$http({
-          url: this.$http.adornUrl('/product/category/list/tree'),
-          method: 'get',
-        }).then(({data}) => {
-          console.log("成功获取到菜单数据", data.data)
-          this.menus = data.data;
-        })
-    }
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        console.log("成功获取到菜单数据", data.data);
+        this.menus = data.data;
+      });
+    },
+    append(data) {
+      console.log("append", data);
+    },
+
+    remove(node, data) {
+      console.log("remove", node, data);
+    },
   },
 };
 </script>
