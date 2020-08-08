bf65c0a4c1a1c044cf17d6f09bca7fd068e12846
Yan <cyiyangg@gmail.com>
2020-08-08 10:57:41 +0800

    ADD: add gateway module

---
+++ fishmall-gateway/src/main/java/com/yan/fishmall/gateway/FishmallGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.yan.fishmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 1. 开启服务注册发现（配置nacos）
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = (DataSourceAutoConfiguration.class))
+public class FishmallGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishmallGatewayApplication.class, args);
+	}
+
+}
---
+++ fishmall-gateway/src/test/java/com/yan/fishmall/gateway/FishmallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yan.fishmall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishmallGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
