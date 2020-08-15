5e86ebe3a90b5283d00969470c9d8fc92605717b
Yan <cyiyangg@gmail.com>
2020-08-15 19:03:26 +0800

    FIX: auto-gen spu save vo from json (86)

---
+++ fishmall-product/src/main/java/com/yan/fishmall/product/vo/Images.java
@@ -0,0 +1,21 @@
+/**
+  * Copyright 2019 bejson.com 
+  */
+package com.yan.fishmall.product.vo;
+
+import lombok.Data;
+
+/**
+ * Auto-generated: 2020-08-15 19:00:00
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+@Data
+public class Images {
+
+    private String imgUrl;
+    private int defaultImg;
+
+
+}
