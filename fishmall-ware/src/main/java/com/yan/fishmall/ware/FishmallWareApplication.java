73a607ff9e86a584d13c9437acfd9b95af912df0
Yan <cyiyangg@gmail.com>
2020-08-16 15:42:56 +0800

    ADD: add ware module (95)

--- fishmall-ware/src/main/java/com/yan/fishmall/ware/FishmallWareApplication.java
+++ fishmall-ware/src/main/java/com/yan/fishmall/ware/FishmallWareApplication.java
@@ -1,9 +1,13 @@
 package com.yan.fishmall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@MapperScan("com.yan.fishmall.ware.dao")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class FishmallWareApplication {
