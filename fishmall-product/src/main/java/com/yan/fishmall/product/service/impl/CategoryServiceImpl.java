5fabf32e126e008abdcc24f971ed83c28e42cad4
Yan <cyiyangg@gmail.com>
2020-09-01 21:34:51 +0800

    FIX: update cache name(169)

--- fishmall-product/src/main/java/com/yan/fishmall/product/service/impl/CategoryServiceImpl.java
+++ fishmall-product/src/main/java/com/yan/fishmall/product/service/impl/CategoryServiceImpl.java
@@ -116,8 +116,25 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         //redis.del("catalogJSON")，等待下次主动查询进行更新
     }
 
-    //每一个需要缓存的数据我们都来指定要放到哪个名字的缓存，【缓存的分区（按照业务类型分）】
-    @Cacheable({"category"})  //代表当前方法的结果需要缓存，如果缓存中有，方法不用调用；如果缓存中没有，调用方法，最后将方法结果放入缓存
+    /**
+     * 1. 每一个需要缓存的数据我们都来指定要放到哪个名字的缓存，【缓存的分区（按照业务类型分）】
+     * 2. @Cacheable({"category"})代表当前方法的结果需要缓存，如缓存有，方法不用调用；如没有，调用后放入缓存
+     * 3.
+     * 3.1 默认行为
+     * 3.1.1 如果缓存中有，方法不调用
+     * 3.1.2 key默认自动生成：缓存的名字::SimpleKey[]
+     * 3.1.3 value的值：默认使用jdk序列化机制
+     * 3.1.4 默认ttl时间：-1，永不过期
+     *
+     * 3.2 自定义：
+     * 3.2.1 指定生成的缓存使用的key，key属性指定：接受一个SpEL语句
+     * 3.2.2 指定缓存的数据的存活时间，配置文件中设置time-to-live
+     * 3.2.3 将数据保存为json格式
+     *
+     */
+
+
+    @Cacheable(value = {"category"}, key= "#root.method.name")  //代表当前方法的结果需要缓存，如果缓存中有，方法不用调用；如果缓存中没有，调用方法，最后将方法结果放入缓存
     @Override
     public List<CategoryEntity> getLevel1Category() {
         System.out.println("getLevel1Categorys..... ");
