2270d2691d4958c40f6dbc0d05a9a045fd3243d8
Yan <cyiyangg@gmail.com>
2021-06-22 21:54:10 +0800

    FIX: update es product index;

--- fishmall-search/src/main/java/com/yan/fishmall/search/constant/EsConstant.java
+++ fishmall-search/src/main/java/com/yan/fishmall/search/constant/EsConstant.java
@@ -2,7 +2,7 @@ package com.yan.fishmall.search.constant;
 
 public class EsConstant {
 
-    public static final String PRODUCT_INDEX = "product"; //sku数据在es中的索引
+    public static final String PRODUCT_INDEX = "fishmall_product"; //sku数据在es中的索引
     public static final Integer PRODUCT_PAGESIZE = 2;
 
 }
